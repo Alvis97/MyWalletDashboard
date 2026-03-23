c65d8cacf81af2cf03b07f4c5d5c1bd35b3052be
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2026-03-23 11:40:55 +0000

    Created ImageWithSceleton component
    -add animation if the image takes long to load

---
+++ components/ImageWithSkeleton.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+function ImageWithSkeleton({ src }: { src: string }) {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div>
+      { !loaded && (
+        <div className='animate-pulse bg-neutral-300 h-[200px] w-[220px] rounded-xs'></div>
+      )}
+      <img 
+      src={src}
+      className={`h-[200px] w-[220px] rounded-xs object-cover ${loaded ? 'block' : 'hidden'}`}
+      alt="NFT image"
+      onLoad={() => setLoaded(true)}
+      onError={(e) => {
+        e.currentTarget.src = '/placeholder.png'
+        e.currentTarget.onerror = null
+        setLoaded(true)
+      }}
+    />  
+    </div>
+  )
+}
+
+export default ImageWithSkeleton
